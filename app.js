a07e9df8a67bdd80cdded9a62f8680a2c0a8054a
artisan <kmgnikmgni@gmail.com>
2022-06-17 03:26:12 +0900

    오류 수정

--- app.js
+++ app.js
@@ -81,6 +81,6 @@ app.use((err, req, res, next) => {
 
 module.exports = app;
 
-// app.listen(app.get('port'), () => {
-//     console.log(app.get('port'), '번 포트에서 대기 중');
-// })
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중');
+})
